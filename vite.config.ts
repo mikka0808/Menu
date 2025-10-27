0c31185173f6f80b03e4bbc662fd00c5a64fc2e0
mikka0808 <51734184+mikka0808@users.noreply.github.com>
2025-10-27 19:44:58 +0100

    Fix module alias resolution for Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
@@ -18,6 +19,11 @@ export default defineConfig({
       }
     })
   ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
   server: {
     port: 5173,
     host: true
